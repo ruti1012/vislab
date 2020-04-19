81587e67bcfe85c39de6c6cdc342194dd461c348
Tim Rüb <ruti1012@hs-karlsruhe.de>
2020-04-19 16:10:17 +0200

    add answers

--- lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
+++ lab1/lab1-example/src/main/java/de/hska/iwi/vislab/lab1/example/TestWsServer.java
@@ -4,6 +4,7 @@ import javax.swing.JOptionPane;
 import javax.xml.ws.Endpoint;
 
 import de.hska.iwi.vislab.lab1.example.ws.BuecherServiceImpl;
+import de.hska.iwi.vislab.lab1.example.ws.FibonacciServiceImpl;
 
 
 /** Testserver fuer den Webservice */
@@ -11,8 +12,13 @@ public class TestWsServer {
 	public static void main(final String[] args) {
 		String url = (args.length > 0) ? args[0]
 				: "http://localhost:4434/buecherservice";
+		String url2 = (args.length > 0) ? args[0]
+				: "http://localhost:4434/fibonacciservice";
 		Endpoint ep = Endpoint.publish(url, new BuecherServiceImpl());
+		Endpoint ep2 = Endpoint.publish(url2, new FibonacciServiceImpl());
+
 		JOptionPane.showMessageDialog(null, "TestWsServer beenden");
 		ep.stop();
+		ep2.stop();
 	}
 }
